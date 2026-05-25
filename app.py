68c942e982757b4d41ac43b1cc4a150878a9738d
Dimpal <dimpalm0321@gmail.com>
2026-05-25 16:01:38 +0530

    increase heatmap visibility

--- app.py
+++ app.py
@@ -82,7 +82,7 @@ def heatmap():
         {
             "lat": lat,
             "lng": lng,
-            "intensity": 0.6
+            "intensity": 0.65
         }
         for lat, lng in points
     ]
